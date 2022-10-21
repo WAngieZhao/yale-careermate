ecf1ed29d282e3a86d838dd6c36275a4e3120113
Angie Zhao <wz1342@nyu.edu>
2022-10-21 14:27:19 -0400

    Implement search result UI

--- packages/frontend/search-components/userSearchResultRow.tsx
+++ packages/frontend/search-components/userSearchResultRow.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image'
-import {Text, Box, HStack, Container, Button, Image as ChakraImage} from "@chakra-ui/react"
-import Link from "next/link";
-import {ArrowForwardIcon} from '@chakra-ui/icons'
-import image from "../assets/building_image.png";
+import {Text, Box,Image as ChakraImage} from "@chakra-ui/react"
+import {Link, Flex, Spacer} from "@chakra-ui/react"
 import React from 'react';
 
 
@@ -16,27 +13,28 @@ type UserBasicProps = {
     picture: string
 }
 
-
 export default function UserSearchResultRow(props: UserBasicProps) {
 
     let {id, email, name, contact_email, status, company, picture} = props;
     return (
         <Box p={2} borderWidth={'3px'} borderRadius={6} m={4} _hover={{bg: "gray.50"}}>
-            <HStack alignItems={"flex-start"} wrap={"wrap"}>
-                <Box p={2} w={"60%"}>
-                    <Box>
-                        <Text fontSize={"3xl"} textTransform="uppercase">{name}</Text>
-                        <Text fontSize={"2xl"}>{`${email}, ${contact_email}, ${company}`}</Text>
-                    </Box>
-                    <Box mt={3} ml={2}> <Link href={`/user/${id}`} passHref={true}><Button
-                        variant='link' rightIcon={<ArrowForwardIcon/>}>Read more</Button></Link></Box>
+                <Box p={2}>
+                <Flex>
+                <Box ml='3'>
+                    <Text fontSize='l'> <Text as='b'><Link href={`/user/${id}`} isExternal> {name}</Link></Text></Text>
+
+                    {/*<Text fontWeight='bold'> {name}</Text>*/}
+                    {/*<Text fontSize='sm'>{company}</Text>*/}
+                    {/*<Badge ml='1' colorScheme='green'>*/}
+                        {/*validateStatus(status)*/}
+                    {/*</Badge>*/}
                 </Box>
-                {/*<Box w={350} py={3}>*/}
-                {/*    {photos.length > 0 ?*/}
-                {/*        <ChakraImage width={350} height={"100%"} src={photos[0]} alt={"building image"}/> :*/}
-                {/*        <Image layout="responsive" src={image} alt={"building image"}/>}*/}
-                {/*</Box>*/}
-            </HStack>
+                <Spacer />
+                <Box ml='3'>
+                    <Text fontSize='l'> {company}</Text>
+                </Box>
+                </Flex>
+                </Box>
         </Box>
     );
 }
