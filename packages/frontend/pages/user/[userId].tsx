24621605d59d90e0c374167c4ab7d2f5a630d11e
stephanieli000131 <44686034+stephanieli000131@users.noreply.github.com>
2022-10-21 14:50:12 -0400

    Implement non-editable user profile page UI

--- packages/frontend/pages/user/[userId].tsx
+++ packages/frontend/pages/user/[userId].tsx
@@ -7,13 +7,11 @@ import CoreLayout from "../../components/coreLayout";
 import NavBar from '../../shared-components/NavBar';
 import {Heading, Button, Text, HStack, Spacer, Divider, Container, useToast} from '@chakra-ui/react';
 import {Box} from "@chakra-ui/react"
-
+import {Image, Avatar, Center, Flex, Badge} from "@chakra-ui/react"
 
 export default function DisplayUser() {
     const router = useRouter()
     const {userId} = router.query
-
-
     const GET_USER = gql`
         query GetUser($userId: ID!) {
             user(id: $userId) {
@@ -34,6 +32,16 @@ export default function DisplayUser() {
         nextFetchPolicy: "network-only"
     });
 
+    function validateStatus(status: boolean) {
+        let curr_status
+        if (status == true) {
+            curr_status = "Alum"
+        } else{
+            curr_status = "Student"
+        }
+        return curr_status
+    }
+
     // if (loading) return <p>Loading...</p>;
     // if (error) return <p>There was an error loading the building data :( ${error.message}</p>;
     if (error) router.push('/search')
@@ -46,43 +54,32 @@ export default function DisplayUser() {
             <Container maxW={"container.lg"}>
                 {/*<a onClick={() => router.back()}>&#8592; Back to search results</a>*/}
                 {/*TODO: design user profile layout*/}
-                {!loading && !error && data && <><Box>{data.user.email}</Box>
-                    <Box>{data.user.name}</Box></>}
+                {!loading && !error && data && <>
+                    <Center h = "50vh" w='1000px'>
+                    <Flex>
+                    <Avatar size= '2xl' src= {data.user.picture} />
+                    <Box ml='10'>
+                        <Text fontSize='2xl' fontWeight='bold'>
+                            {data.user.name}
+                            <Badge ml='1' colorScheme='green'>
+                                {validateStatus(data.user.status)}
+                            </Badge>
+                        </Text>
+                        <br />
+                        <Text fontSize='l'> <Text as='b'>Company : </Text>{data.user.company}</Text>
+                        <br />
+                        <Text fontSize='l'> <Text as='b'>Email : </Text>{data.user.email}</Text>
+                        <br />
+                        <Text fontSize='l'><Text as='b'>Contact Email : </Text>{data.user.contact_email}</Text>
+                    </Box>
+                    </Flex>
+                    </Center>
 
-                {/*<HStack wrap={"wrap"}>*/}
-                {/*    <Heading my={3} as='h1' size='3xl'>{building.buildingName}</Heading> <Spacer/>*/}
-                {/*    <Link href={{*/}
-                {/*        pathname: "/reviewForm",*/}
-                {/*        query: {*/}
-                {/*            building: building.id,*/}
-                {/*            buildingName: building.buildingName*/}
-                {/*        },*/}
-                {/*    }} passHref={true}>*/}
-                {/*        <Button colorScheme='brand'>Leave a review</Button>*/}
-                {/*    </Link>*/}
-                {/*</HStack>*/}
 
-                {/*<Heading mt={2} as='h2' size='xl'>{building.buildingAddress}</Heading>*/}
-                {/*<Box m={3}>*/}
-                {/*    <ScoreTags total={building.totalScore} secure={building.secScore} environment={building.envScore}/>*/}
-                {/*</Box>*/}
+                </>}
 
-
-                {/*<Heading mt={5} as='h2' size='lg' color={"gray.500"}>Comments</Heading>*/}
-                {/*<Divider p={1} mb={5}/>*/}
             </Container>
         </CoreLayout>
     )
 }
 
-// function renderFeatureList(featureList: string[]) {
-//     return <ul>{renderFeatureListItems(featureList)}</ul>
-// }
-//
-// function renderFeatureListItems(featureList: string[]) {
-//     return featureList.map((feature: string, index: number) => (
-//         <li key={index}>
-//             {feature}
-//         </li>
-//     ))
-// }
